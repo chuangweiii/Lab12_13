4b22b030656f2b1dffd506047aad9d7857982355
chuangweiii <w9201200007@gmail.com>
2023-12-11 22:49:48 +0800

    Lab12_13

---
+++ Lab12/app/src/main/java/com/example/lab12/SecActivity.kt
@@ -0,0 +1,12 @@
+package com.italkutalk.lab12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lab12.R
+
+class SecActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sec)
+    }
+}
